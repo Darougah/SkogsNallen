01bbcfd99123b7c0238ffc6b11645947652eb37c
dadar870218 <dadar870218@gmail.com>
2025-03-25 11:14:52 +0100

    removed $ sign

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -22,7 +22,7 @@ function ProductCards({products}) {
 {/* product desc */}
 <div className='product__card__content'>
   <h4>{product.name}</h4>
-  <p>${product.price}{product.oldPrice ? <s>${product?.oldPrice}</s>:null}</p>
+  <p>{product.price}{product.oldPrice ? <s>{product ?.oldPrice}</s>:null}</p>
 
 <RatingStars rating={product.rating}/>
 </div>
