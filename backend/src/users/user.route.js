9237cc5b7ab5084b3e359f96c493d12988091fc1
dadar870218 <dadar870218@gmail.com>
2025-04-14 12:39:35 +0200

    add login routes

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -10,8 +10,33 @@ router.post('/register', async(req, res)=>{
   await user.save()
   res.status(201).send({message: "User registered successfully!"})
   } catch (error) {
+    console.log("Error registering user");
+    res.status(500).send({message:"Error registering user"})
     
   }
 })
+
+
+//login user endpoint
+router.post('/login' , async (req,res)=>{
+const {email, password} = req.body;
+const user = await User.findOne({email})
+try {if(!user){
+  return res.status(404).send({message:'User not found'})
+}
+const isMatch = await user.comparePassword(password)
+  if(!isMatch){
+    return res.status(401).send({message:'password not match'})
+  }
+  
+  res.status(200).send({message:"Logged in successfully", user})
+  
+} catch (error) {
+  console.log("Error logged in user");
+  res.status(500).send({message:"Error logged in user"})
+}
+
+})
+
 
 module.exports= router;
