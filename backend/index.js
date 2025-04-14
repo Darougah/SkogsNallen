fbbd6eac9927237c0cee0c6773537c2ed8cfbcb9
dadar870218 <dadar870218@gmail.com>
2025-04-14 12:14:45 +0200

    Add user Schema, model and routes.. add hash password

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,11 @@ app.use(
   })
 );
 
+
+//all routes
+const authRoutes = require('./src/users/user.route');
+app.use('/api/auth',authRoutes)
+
 main()
   .then(() => console.log("Mongodb is successfuly connected"))
   .catch((err) => console.log(err));
