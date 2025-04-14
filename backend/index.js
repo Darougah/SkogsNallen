052ac5ecb753dfcdda3f3e3dbaea8e48afea9bea
dadar870218 <dadar870218@gmail.com>
2025-04-14 10:38:52 +0200

    connect mongodb, install mongoose,dotenx.cors , cookie parser and body parser

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const port = process.env.Port || 5000;
 
-app.get('/', (req, res) => {
-  res.send('SkogsNallens butik körs...!')
-})
+//middleware
+app.use(express.json({ limit: "25mb" }));
+app.use((express.urlencoded({ limit: "25mb" })))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
+
+main()
+  .then(() => console.log("Mongodb is successfuly connected"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+
+  app.get("/", (req, res) => {
+    res.send("SkogsNallens butik körs...!");
+  });
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
