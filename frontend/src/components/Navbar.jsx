b0bfd5eb8fbfca4b57b95172638680007a731c98
dadar870218 <dadar870218@gmail.com>
2025-04-28 15:23:15 +0200

    add dropdown menu for admin and users when logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import CartModal from '../pages/shop/CartModal';
+import avatarImg from "../assets/avatar.png"
+import { useLogoutUserMutation } from '../redux/features/auth/authApi';
+import { logout } from '../redux/features/auth/authSlice';
 
 const Navbar = () => {
   const products = useSelector((state)=>state.cart.products);
@@ -10,6 +13,48 @@ const Navbar = () => {
   const handleCartToggle = () => {
     setisCartOpen(!isCartOpen)
   }
+
+const dispatch = useDispatch();
+const {user}= useSelector((state)=> state.auth)
+// console.log(user)
+
+const [logoutUser] = useLogoutUserMutation();
+const navigate = useNavigate()
+
+//Dropdown menu
+const [isDropDownOpen, setIsDropDownOpen] = useState (false)
+const handDropDownToggle = () =>{
+  setIsDropDownOpen(!isDropDownOpen)
+}
+
+//admin dropdown
+const adminDropDownMenus =[
+  {label:"Adminpanel", path:"/dashboard/admin"},
+  {label:"Hantera Produkter", path:"/dashboard/manage-products"},
+  {label:"Alla Beställningar", path:"/dashboard/manage-orders"},
+  {label:"Skapa Ny", path:"/dashboard/add-new-post"},
+]
+
+//user dropdown
+const userDropDownMenus= [
+  {label:"Mitt Konto", path:"/dashboard"},
+  {label:"Profil", path:"/dashboard/profile"},
+  {label:"Betalningar", path:"/dashboard/payments"},
+  {label:"Beställningar", path:"/dashboard/orders"},
+]
+
+const dropDownMenus = user?.role ==='admin' ? [...adminDropDownMenus]: [...userDropDownMenus]
+
+const handleLogout = async () => {
+  try {
+    await logoutUser().unwrap();
+    dispatch(logout());
+    navigate("/");
+  } catch (error) {
+    console.error("Misslyckades med att logga ut", error);
+  }
+};
+
   return (
     <header className='fixed-nav-bar w-nav'>
     <nav className='max-w-screen-2x1 mx-auto px-4 flex justify-between items-center'>
@@ -42,10 +87,44 @@ const Navbar = () => {
         <sup className='text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center'>{products.length}</sup>
         </button>
       </span>
-      <span>
-        <Link to="login">
-        <i className="ri-user-line"></i></Link>
-      </span>
+<span>
+{
+  user ? (
+    <>
+      <img 
+        onClick={handDropDownToggle}
+        src={user?.profileImage || avatarImg} 
+        alt='' 
+        className="size-6 rounded-full cursor-pointer" 
+      />
+
+      {isDropDownOpen && (
+        <div className='absolute right-0 mt-3 p-4 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50'>
+          <ul className='font-medium space-y-4 p-2'>
+            {dropDownMenus.map((menu, index) => (
+              <li key={index}>
+                <Link 
+                  onClick={() => setIsDropDownOpen(false)}
+                  className='dropdown-items' 
+                  to={menu.path}
+                >
+                  {menu.label}
+                </Link>
+              </li>
+            ))}
+            <li><Link onClick={handleLogout} className='dropdown-items'>Logout</Link></li>
+          </ul>
+        </div>
+      )}
+    </>
+  ) : (
+    <Link to="/login">
+      <i className="ri-user-line"></i>
+    </Link>
+  )
+}
+
+</span>
     </div>
 
     </nav>
