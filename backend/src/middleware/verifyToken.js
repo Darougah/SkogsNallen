f19ceae1750c23879930349ececcc2a8eedb7409
dadar870218 <dadar870218@gmail.com>
2025-04-30 13:13:26 +0200

    add admin middleware , token work

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
 const verifyToken = (req, res, next)=>{
   try {
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
     // console.log("Verify token:",token)
+    
+    const authHeader = req.headers["authorization"];
+const token = authHeader && authHeader.split(" ")[1];
 
     if(!token){
       return res.status(401).send({message:'Invalid token'})
