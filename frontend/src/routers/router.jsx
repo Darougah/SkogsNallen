f983dc6fc3955a22924c3c0ce0f2f31022c71e9e
dadar870218 <dadar870218@gmail.com>
2025-05-05 15:02:43 +0200

    add dashboard and user/admin route

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,33 +8,59 @@ import SingleProduct from "../pages/shop/productDetails/SingleProduct";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import PaymentSuccess from "../components/PaymentSuccess";
-
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
-      {path:"/",element: <Home/>},
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
       { path: "/categories/:categoryName", element: <CategoryPage /> },
-      {path:"/search",element: <Search/>},
-      {path:"/shop",element: <ShopPage/>},
+      { path: "/search", element: <Search /> },
+      { path: "/shop", element: <ShopPage /> },
       { path: "/shop/:id", element: <SingleProduct /> },
       {
         path: "/success",
-        element: <PaymentSuccess/>
-      }
-      
-    ]
+        element: <PaymentSuccess />,
+      },
+      {
+        path: "/dashboard",
+        element: <PrivateRoute> <DashboardLayout /></PrivateRoute>,
+        children: [
+          //user routes
+          { path: "",element: <div>User Dashboard</div>},
+          { path: "orders",element: <div>User Order</div>},
+          { path: "payments",element: <div>User Payments</div>},
+          { path: "profile",element: <div>User Profile</div>},
+          { path: "reviews",element: <div>User Reviews</div>},
+
+
+
+
+
+          //admin routes
+
+          { path: "admin", element: <PrivateRoute role="admin"><div>Admin Dashboard</div></PrivateRoute> },
+          { path: "add-new-post", element: <PrivateRoute role="admin"><div>New Post</div></PrivateRoute> },
+          { path: "manage-products", element: <PrivateRoute role="admin"><div>Manage Products</div></PrivateRoute> },
+          { path: "update-product/:id", element: <PrivateRoute role="admin"><div>Update Product</div></PrivateRoute> },
+          { path: "users", element: <PrivateRoute role="admin"><div>All Users</div></PrivateRoute> },
+          { path: "manage-orders", element: <PrivateRoute role="admin"><div>Manage Orders</div></PrivateRoute> },
+          
+        ],
+      },
+    ],
   },
   {
-    path:"/login",
-    element:<Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path:"/register",
-    element:<Register/>
-  }
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 export default router;
