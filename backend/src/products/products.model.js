f97c18e351d240eaaf606dce48975d849dd651ca
dadar870218 <dadar870218@gmail.com>
2025-05-08 17:37:53 +0200

    add time

--- backend/src/products/products.model.js
+++ backend/src/products/products.model.js
@@ -16,6 +16,9 @@ const ProductSchema = new mongoose.Schema ({
     type: Number, default:0
   },
   author: {type: mongoose.Types.ObjectId,ref:"User", required:true }
+},
+{
+  timestamps:true
 })
 
 const Products = mongoose.model("Product", ProductSchema);
