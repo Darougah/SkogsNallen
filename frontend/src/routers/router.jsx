6d851bfa9680f5ee9d62aaafc2a8c1183e3c63c4
dadar870218 <dadar870218@gmail.com>
2025-05-08 17:39:27 +0200

    add ManageProduct , UpdateProduct

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -18,6 +18,8 @@ import UserReviews from "../pages/dashboard/user/UserReviews";
 import UserProfile from "../pages/dashboard/user/UserProfile";
 import AdminDMain from "../pages/dashboard/admin/dashboard/AdminDMain";
 import AddProduct from "../pages/dashboard/admin/addProduct/AddProduct";
+import ManageProduct from "../pages/dashboard/admin/manageProduct/ManageProduct";
+import UpdateProduct from "../pages/dashboard/admin/manageProduct/UpdateProduct";
 
 const router = createBrowserRouter([
   {
@@ -51,8 +53,8 @@ element:<OrderDetails/>
           //admin routes
           { path: "admin", element: <PrivateRoute role="admin"><AdminDMain/></PrivateRoute> },
           { path: "add-product", element: <PrivateRoute role="admin"><AddProduct/></PrivateRoute> },
-          { path: "manage-products", element: <PrivateRoute role="admin"><div>Hantera Produkter</div></PrivateRoute> },
-          { path: "update-product/:id", element: <PrivateRoute role="admin"><div>Uppdatera Produkt</div></PrivateRoute> },
+          { path: "manage-products", element: <PrivateRoute role="admin"><ManageProduct/></PrivateRoute> },
+          { path: "update-product/:id", element: <PrivateRoute role="admin"><UpdateProduct/></PrivateRoute> },
           { path: "users", element: <PrivateRoute role="admin"><div>Alla Användare</div></PrivateRoute> },
           { path: "manage-orders", element: <PrivateRoute role="admin"><div>Hantera Beställningar</div></PrivateRoute> },
           
