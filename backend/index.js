61e03a68c85236357a778d346c72769e3040c7db
dadar870218 <dadar870218@gmail.com>
2025-05-08 20:07:03 +0200

    fix port to caps

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json({ limit: "25mb" }));
