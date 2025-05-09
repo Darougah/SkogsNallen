1bd87aa8e62a6179f6301fde15f6053c55cbab22
dadar870218 <dadar870218@gmail.com>
2025-05-09 15:12:04 +0200

    fix error total products shown in adminPanel

--- frontend/src/pages/dashboard/admin/dashboard/AdminStats.jsx
+++ frontend/src/pages/dashboard/admin/dashboard/AdminStats.jsx
@@ -21,7 +21,7 @@ const AdminStats = ({ stats }) => {
 
         <div className="bg-white shadow-md rounded-lg p-6 border border-gray-200 hover:scale-105 transition-transform duration-200 cursor-pointer">
           <h2 className="text-xl font-semibold mb-2">Totalt antal produkter</h2>
-          <p className="text-2xl font-bold">{stats?.total ?? 0}</p>
+          <p className="text-2xl font-bold">{stats?.totalProducts ?? 0}</p>
         </div>
       </div>
     </div>
