66b0b53176097c2bede34b8f6f74343c6b64c126
dadar870218 <dadar870218@gmail.com>
2025-05-10 11:00:39 +0200

    fix the path , defined routes

--- frontend/src/pages/home/Categories.jsx
+++ frontend/src/pages/home/Categories.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import category1 from "../../assets/nalle.png";    
-import category2 from "../../assets/trä-leksaker.png";   
-import category3 from "../../assets/pussel-bygg.png";  
-import category4 from "../../assets/pussel-bygg.png";      
-import category5 from "../../assets/babykläder.png";      
-import category6 from "../../assets/böcker.png";      
-import category7 from "../../assets/figurer.png";    
-import category8 from "../../assets/fordon.png";    
+import category1 from "../../assets/nalle.png";
+import category2 from "../../assets/trä-leksaker.png";
+import category3 from "../../assets/pussel-bygg.png";
+import category4 from "../../assets/pussel-bygg.png";
+import category5 from "../../assets/babykläder.png";
+import category6 from "../../assets/böcker.png";
+import category7 from "../../assets/figurer.png";
+import category8 from "../../assets/fordon.png";
+
+const categories = [
+  { name: "Gosedjur", path: "gosedjur", image: category1 },
+  { name: "Träleksaker", path: "trä-leksaker", image: category2 },
+  { name: "Pussel & Bygg", path: "pussel-bygg", image: category3 },
+  { name: "Spel & Pyssel", path: "spel-pyssel", image: category4 },
+  { name: "Babykläder", path: "babykläder", image: category5 },
+  { name: "Böcker", path: "böcker", image: category6 },
+  { name: "Figurer", path: "figurer", image: category7 },
+  { name: "Fordon", path: "fordon", image: category8 }
+];
 
 const Categories = () => {
-  const categories = [
-    { name: "Gosedjur", path: "gosedjur", image: category1 },
-    { name: "Träleksaker", path: "trä-leksaker", image: category2 },
-    { name: "Pussel & Bygg", path: "pussel-bygg", image: category3 },
-    { name: "Spel & Pyssel", path: "spel-pyssel", image: category4 },
-    { name: "Babykläder", path: "babykläder", image: category5 },
-    { name: "Böcker", path: "böcker", image: category6 },
-    { name: "Figurer", path: "figurer", image: category7 },
-    { name: "Fordon", path: "fordon", image: category8 }
-  ];
-
   return (
     <div className="product__grid">
       {categories.map((category) => (
@@ -31,7 +31,7 @@ const Categories = () => {
             alt={category.name}
             className="w-full h-auto object-cover rounded-lg shadow-md"
           />
-          <h4 className="text-lg font-semibold mt-2 text-text-dark">{category.name}</h4>
+          <h4 className="text-lg font-semibold mt-2 text-text-dark capitalize">{category.name}</h4>
         </Link>
       ))}
     </div>
--- frontend/src/pages/home/HeroSection.jsx
+++ frontend/src/pages/home/HeroSection.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import card1 from "../../assets/aligned_toy_1.png"
-import card2 from "../../assets/aligned_toy_4.png"
-import card3 from "../../assets/aligned_toy_3.png"
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import card1 from "../../assets/aligned_toy_1.png";
+import card2 from "../../assets/aligned_toy_4.png";
+import card3 from "../../assets/aligned_toy_3.png";
 
 const cards = [
   {
@@ -9,39 +11,39 @@ const cards = [
     image: card1,
     trend: 'Populärt just nu',
     title: 'Spel & Pyssel',
+    path: 'spel-pyssel',
   },
   {
     id: 2,
     image: card2,
     trend: 'Populärt just nu',
     title: 'Gosedjur',
+    path: 'gosedjur',
   },
   {
     id: 3,
     image: card3,
     trend: 'Populärt just nu',
     title: 'Träleksaker',
+    path: 'trä-leksaker',
   },
 ];
 
 const HeroSection = () => {
   return (
     <section className='section__container hero__container'>
-      {
-        cards.map((card)=>(
-          <div key={card.id} className='hero__card'>
-            <img src={card.image} alt=''/>
-            <div className='hero__content'>
-              <p>{card.trend}</p>
-              <h4>{card.title}</h4>
-              <a href='#'>Ta en titt</a>
-            </div>
-
+      {cards.map((card) => (
+        <div key={card.id} className='hero__card'>
+          <img src={card.image} alt={card.title} />
+          <div className='hero__content'>
+            <p>{card.trend}</p>
+            <h4>{card.title}</h4>
+            <Link to={`/categories/${card.path}`}>Ta en titt</Link>
           </div>
-        ))
-      }
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
