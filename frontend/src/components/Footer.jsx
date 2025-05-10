b7ad4ddae509282ca05e49694815b78c5d97b2f6
dadar870218 <dadar870218@gmail.com>
2025-05-10 10:50:00 +0200

    add footer link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,7 @@
+
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -19,20 +22,22 @@ const Footer = () => {
                         (+46) 73-613 00 70
                     </p>
                 </div>
+
                 <div className='footer__col'>
                     <h4>OM SKOGSNALLEN</h4>
-                    <a href="/">Hem</a>
-                    <a href="/">Om Oss</a>
-                    <a href="/">Jobba med oss</a>
-                    <a href="/">Villkor & Regler</a>
+                    <Link to="/">Hem</Link>
+                    <Link to="/pages/about">Om Oss</Link>
+                    <Link to="/pages/careers">Jobba med oss</Link>
+                    <Link to="/pages/terms">Villkor & Regler</Link>
                 </div>
+
                 <div className='footer__col'>
                     <h4>SUPPORT</h4>
-                    <a href="/">Hjälp & Support</a>
-                    <a href="/">Spåra din beställning</a>
-
+                    <Link to="/pages/help">Hjälp & Support</Link>
+                    <Link to="/dashboard/orders">Spåra din beställning</Link>
                 </div>
             </footer>
+
             <div className='footer__bar'>
                 Upphovsrätt © 2025 av SkogsNallen. Alla rättigheter förbehållna.
             </div>
