3c136a391cb6841fd0bf1b005bfdae770c14d5f1
dadar870218 <dadar870218@gmail.com>
2025-05-11 18:51:35 +0200

    config render

--- backend/src/orders/orders.route.js
+++ backend/src/orders/orders.route.js
@@ -27,8 +27,8 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:5173/cancel`,
+      success_url: `https://skogsnallen-1.onrender.com/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://skogsnallen-1.onrender.com/cancel`,
     });
 
     res.json({ id: session.id });
